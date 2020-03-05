8b6c73774a02e798df170b416e9d8462df626880
Rishabh Raj Jaiswal <deliberatethink@gmail.com>
2020-03-05 15:06:26 +0530

    Create MainActivity.java

---
+++ webView/MainActivity.java
@@ -0,0 +1,36 @@
+    <uses-permission android:name="android.permission.INTERNET"/>
+
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <color name="colorPrimary">#000000</color>
+    <color name="colorPrimaryDark">#000000</color>
+    <color name="colorAccent">#000000</color>
+</resources>
+
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    final String url = "https://thebestlife.herokuapp.com";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
