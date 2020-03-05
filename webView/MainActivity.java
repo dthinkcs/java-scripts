59c59b3b6e94e169eb30acb22f7128005b2dbda2
Rishabh Raj Jaiswal <deliberatethink@gmail.com>
2020-03-05 15:11:54 +0530

    Update MainActivity.java

--- webView/MainActivity.java
+++ webView/MainActivity.java
@@ -7,6 +7,21 @@
     <color name="colorAccent">#000000</color>
 </resources>
 
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".MainActivity">
+
+    <WebView
+        android:id="@+id/webView"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        tools:layout_editor_absoluteX="1dp"
+        tools:layout_editor_absoluteY="1dp" />
+</androidx.constraintlayout.widget.ConstraintLayout>
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
